022599c91ad4b665356525c79db91c471c0abd4f
Jan Růžička <jan.ruzicka01@gmail.com>
2021-08-12 20:50:19 +0200

    docs

--- src/expand_impl.rs
+++ src/expand_impl.rs
@@ -1,3 +1,5 @@
+/// Accepts either `$expr`, or `use { $path, $path, ... } in $expr`.
+/// Expands the namespaces of `$path`s inside `$expr`.
 #[macro_export]
 macro_rules! expand {
     ( $( use { $( $name:path ),* } in )? $val:expr ) => ({
